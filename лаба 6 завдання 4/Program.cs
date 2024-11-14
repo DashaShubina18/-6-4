0ce6b03a25a7ba01a368cbbca6bad6e801154cc3
Даша Шубіна <shubinad268@gmail.com>
2024-11-14 10:24:15 +0200

    Add project files.

---
+++ лаба 6 завдання 4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лаба_6_завдання_4
+{
+    internal class Program
+
+
+    {
+        public static void ReverseNumber(int n)
+        {
+            if (n > 0)
+            {
+                Console.Write(n % 10);
+                ReverseNumber(n / 10);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input n");
+            int n = int.Parse(Console.ReadLine());
+            ReverseNumber(n);
+            Console.WriteLine();
+        }
+    }
+
+}
+    
+
